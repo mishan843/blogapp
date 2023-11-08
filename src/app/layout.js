6fd3201ed50c381990c5fe589665f703a47f76d6
Jenish <kavathiyajenish009@gmail.com>
2023-11-08 23:56:32 +0530

    add logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,8 +8,7 @@ import ThemeProvider from "@/providers/ThemeProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Blog App",
-  description: "The best blog app!",
+
 };
 
 export default function RootLayout({ children }) {
