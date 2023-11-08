aaebdac43b298c65dafca0ffb96bc8b67bc75a6c
Jenish <kavathiyajenish009@gmail.com>
2023-11-08 11:32:52 +0530

    chnages in css

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -6,13 +6,10 @@ import MenuCategories from "../menuCategories/MenuCategories";
 const Menu = () => {
   return (
     <div className={styles.container}>
-      <h2 className={styles.subtitle}>{"What's New"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
       <MenuPosts withImage={true} />
-      <h2 className={styles.subtitle}>Discover by Category</h2>
       <h1 className={styles.title}>Categories</h1>
       <MenuCategories />
-      <h2 className={styles.subtitle}>Chosen by the Editor</h2>
       <h1 className={styles.title}>Editors Pick</h1>
       <MenuPosts withImage={true} />
     </div>
