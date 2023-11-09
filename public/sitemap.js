f4f58038f823a3ce649ee5a8cb909b461781e9f8
Jenish <kavathiyajenish009@gmail.com>
2023-11-09 23:19:32 +0530

    add site map url

---
+++ public/sitemap.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+const fs = require('fs');
+const filePath = 'public/sitemap.txt';
+
+const apiUrlBlog = 'https://blogapp-q8b0.onrender.com/blog/getAllBlogs'
+
+const sitemap = [
+    'https://www.bloggersground.com',
+];
+
+async function fetchData(apiUrl) {
+    try {
+        const response = await axios.get(apiUrl);
+        return response.data.data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
+}
+
+function writeSitemapToFile(sitemap, filePath) {
+    const fileStream = fs.createWriteStream(filePath);
+    sitemap.forEach(url => {
+        fileStream.write(url + '\n');
+    });
+    fileStream.end();
+    fileStream.on('finish', () => {
+        console.log('Sitemap data has been written to sitemap.txt');
+    });
+    fileStream.on('error', (err) => {
+        console.error('Error writing sitemap data:', err);
+    });
+}
+
+async function createNewSiteMap() {
+    try {
+
+        let blogData = await fetchData(apiUrlBlog)
+        console.log(blogData)
+        const blogLinks = blogData.map(obj => `https://www.bloggersground.com/blog/${obj.title.toLowerCase().replace(/\s+/g, "-")}/${obj._id}`);
+
+        sitemap.push(...blogLinks);
+
+        writeSitemapToFile(sitemap, filePath);
+    } catch (error) {
+        console.log("error>>", error);
+    }
+}
+
+createNewSiteMap();
