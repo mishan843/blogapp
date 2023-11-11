d389bd4d216435165e2890ba822fec89dd1d294a
mishan843 <136816870+mishan843@users.noreply.github.com>
2023-11-11 14:44:13 +0530

    url

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -53,7 +53,7 @@ const Card = (body) => {
               <p className={styles.desc}>
                 {item.description}
               </p>
-              <Link href={`/blog/getblogbyid?id=${item._id}`} className={`${styles.link} ${categoryColor}`}>
+              <Link href={`/blog/${title}/getblogbyid?id=${item._id}`} className={`${styles.link} ${categoryColor}`}>
                 Read More
               </Link>
             </div>
@@ -63,6 +63,7 @@ const Card = (body) => {
       }) :
         blogs.map((item) => {
           const categoryColor = getColors(item.category[0]);
+          const title = item.title.toLowerCase().replace(/\s+/g, "-")
 
           return (
             <div key={item.id} className={styles.container}>
@@ -74,13 +75,13 @@ const Card = (body) => {
                   <span className={`${categoryColor} ${styles.category}`}>{item.category}</span>
                   <span className={styles.date}>{item.createdAt}</span>
                 </div>
-                <Link href={`/blog/${item.title.toLowerCase().replace(/\s+/g, "-")}/getblogbyid?id=${item._id}`}>
+                <Link href={`/blog/${title}/getblogbyid?id=${item._id}`}>
                   <h1>{item.title}</h1>
                 </Link>
                 <p className={styles.desc}>
                   {item.description}
                 </p>
-                <Link href={`/blog/getblogbyid?id=${item._id}`} className={styles.link}>
+                <Link href={`/blog/${title}/getblogbyid?id=${item._id}`} className={styles.link}>
                   Read More
                 </Link>
               </div>
