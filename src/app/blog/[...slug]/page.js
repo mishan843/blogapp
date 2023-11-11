74a9d14b42665189a02de25379d629c569d53919
Jenish <kavathiyajenish009@gmail.com>
2023-11-12 01:26:01 +0530

    add popular key

--- src/app/blog/[...slug]/page.js
+++ src/app/blog/[...slug]/page.js
@@ -17,7 +17,7 @@ const Page = () => {
 
   const [singleBlog, setSingleBlog] = useState([]);
   const pageTitle = singleBlog.title
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -49,15 +49,17 @@ const Page = () => {
         <div className={styles.textContainer}>
           <h1 className={styles.title}>{singleBlog.title}</h1>
         </div>
-        <div className={styles.user}>
+        <a href="/"> <div className={styles.user}>
           <div className={styles.userImageContainer}>
             <img src="/profile_logo.jpg" alt="" fill className={styles.avatar} />
           </div>
           <div className={styles.userTextContainer}>
-            <b><span className={styles.username}>Blogger Ground</span></b>
+            <img src="/logo.png" alt="" fill className={styles.avtar_logo} />
+
             <span className={styles.date}>written by</span>
           </div>
         </div>
+        </a>
         <div className={styles.imageContainer}>
           <img src={singleBlog.coverImage} alt="" fill className={styles.image} />
         </div>
