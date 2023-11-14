e7da913a0d03af0397bcd52505731a2e014fad92
mishan843 <136816870+mishan843@users.noreply.github.com>
2023-11-14 10:24:54 +0530

    privacy and contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,7 +4,15 @@ import styles from './contact.module.css';
 const ContactPage = () => {
   return (
     <div className={styles.container}>
-        
+          <h2>Contact Us</h2>
+        <div>
+          <form className={styles.contactform}>
+          <input type="text" placeholder='Name' />
+          <input type="email" placeholder='Email'/>
+          <textarea type="text" placeholder='Message' rows={4}/>
+          <button>Submit</button>
+          </form>
+        </div>
     </div>
   )
 }
