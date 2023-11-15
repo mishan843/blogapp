23b864d033f32145507385ff5f4a137cf07ecd46
manthan123123 <mauliksavani222@gmail.com>
2023-11-15 15:51:12 +0530

    add meta tag

--- src/app/blog/[...slug]/page.js
+++ src/app/blog/[...slug]/page.js
@@ -1,24 +1,54 @@
-
 import SingleBlog from "@/components/singleBlog/SingleBlog";
 
 
-export default function Page(){
 
-  return (
-   <div>
-    <SingleBlog />
-   </div>
-  );
+export const generateMetadata = ({ searchParams, params }) => {
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/blog/getblogbyid/?id=${searchParams.id}`
+      );
+
+      if (response.ok) {
+
+        let result = await response.json();
+         return {
+           title: `${result.title}`,
+           description: `${result.description.slice(0, 60)}`,
+           openGraph: {
+             title: `${result.title}`,
+             description: `${result.description.slice(0, 60)}`,
+             url: `https://www.bloggersground.com/blog/${result.title
+               .toLowerCase()
+               .replace(/\s+/g, "-")}/${result._id}`, // Uncomment this line
+             siteName: "bloggersGround",
+             images: [
+               {
+                 url: `${result.coverImage}`,
+                 width: 1200,
+                 height: 600,
+               },
+             ],
+           },
+         };
+      } else {
+        console.error("Error fetching data:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+ return fetchData();
+
 };
 
-export function generateMetadata() {
-  // Check if running on the client side
-  if (typeof window !== 'undefined') {
-    const status = localStorage.getItem("blog");
-    const blogData = JSON.parse(status);
-    return {
-      title: (blogData.title),
-      description: `sdfsdfsfsdfdsf`
-    };
-  }
+export default function Page({ result }) {
+  return (
+    <div>
+      <SingleBlog data={result} />
+    </div>
+  );
 }
+
+
