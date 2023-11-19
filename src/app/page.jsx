f5a045c588950e9f45f160e2ff518145508ecc03
Jenish <kavathiyajenish009@gmail.com>
2023-11-19 17:20:52 +0530

    add canone url

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,11 +10,12 @@ export const metadata = {
   },
   description:
     "Bloggers Ground - A platform designed specifically for bloggers. Connect with us, access helpful services, and grow your blog",
+  keywords: ["Fashion blogs", "travel blogs", "finance blog", "blogging"],
   openGraph: {
     title: "bloggersGround",
     description:
       "Bloggers Ground - A platform designed specifically for bloggers. Connect with us, access helpful services, and grow your blog",
-    url: "bloggersground.com",
+    url: process.env.DOMAIN,
     siteName: "bloggersGround",
     images: [
       {
@@ -22,7 +23,34 @@ export const metadata = {
         width: 1200,
         height: 600,
       },
+      {
+        url: "https://drive.google.com/uc?id=1ctCe5W1DBcxfsRrSdjUp6avfwCZ6lUBd",
+        width: 1080,
+        height: 1080, // Instagram square image size
+      },
+      {
+        url: "https://drive.google.com/uc?id=1ctCe5W1DBcxfsRrSdjUp6avfwCZ6lUBd",
+        width: 600,
+        height: 315, // Twitter share image size
+      },
+      {
+        url: "https://drive.google.com/uc?id=1ctCe5W1DBcxfsRrSdjUp6avfwCZ6lUBd",
+        width: 540,
+        height: 540, // WhatsApp share image size
+      },
     ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `bloggersGround`,
+    description: `bloggersGround`,
+    creator: "bloggersGround",
+    images: [
+      `https://drive.google.com/uc?id=1ctCe5W1DBcxfsRrSdjUp6avfwCZ6lUBd`,
+    ],
+  },
+  alternates: {
+    canonical: `${process.env.DOMAIN}`,
   },
 };
 export default function Home() {
