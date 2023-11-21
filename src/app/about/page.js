b098566d415ac9fe1fa0f61f9be452393e082b56
mishan843 <136816870+mishan843@users.noreply.github.com>
2023-11-21 14:44:59 +0530

    about us page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,93 @@
-import React from 'react'
-import styles from './about.module.css'
+import React from "react";
+import styles from "./about.module.css";
 const AboutPage = () => {
   return (
     <div className={styles.container}>
-        
+      <h1>About Us</h1>
+      <h3>Welcome to BloggersGround!</h3>
+      <p>
+        At <span>BloggersGround</span>, we are not just a blog website, we are a vibrant
+        community fueled by a shared passion for Many Category like Style,
+        Travel, Coding, Food, Fashion and Finance. Our journey began with a
+        simple idea – to create a platform where enthusiasts, beginners, and
+        experts alike could come together to explore, learn, and celebrate the
+        wonders of our Content about this Category.
+      </p>
+      <h3>Our Mission</h3>
+      <p>
+        Our mission is deeply rooted in People more learn about Category like
+        Style, Travel, Coding, Food, Fashion and Finance so We can Provide more
+        Content about this and knowledge from our experience and Content Writer
+        Team. We strive to be the go-to resource for Top 10 valuable best
+        content and blogger community that, offering not only valuable
+        information but also fostering a sense of connection and inspiration
+        within our community.
+      </p>
+      <h3>What We Offer</h3>
+      <ul>
+        <li>
+          <span>In-Depth Articles:</span>Immerse yourself in a treasure trove of
+          knowledge with our meticulously crafted articles. From Finance,
+          Fashion, Food, Coding, Style and many more to deep in blogs Content,
+          we cover it all with the depth and insight you crave.
+        </li>
+        <li>
+          <span>Expert Advice:</span>Our team of seasoned write blog with thier
+          experince and search proper data about the blog experts is dedicated
+          to bringing you practical insights and advice based on years of
+          hands-on experience. Learn from the best to elevate your blog journy
+          with our experies and learn from our blogs that help you to success
+          your journy journey.
+        </li>
+        <li>
+          <span>Community Engagement:</span>Join a community that thrives on
+          expertise. Our forums are buzzing with discussions led by our team
+          members, where you can interact directly with experts in the field.
+        </li>
+      </ul>
+      <h3>Meet the Expert Team</h3>
+      <h4>Jenish Kavathiya - Founder and Chief Editor</h4>
+      <p>
+        Greetings! I'm Jenish Kavathiya, the founder and chief editor of
+        &apos;BloggersGround.com&apos;. My journey in make this Blogging website
+        began when I realized that what i faced when i was in this middle of
+        journy that should not face this problem, So I make this amazing website
+        &apos;BloggersGround.com&apos;. As a Founder and Person that enthusiast
+        with a background in IT field, my goal is to bridge the gap between
+        expertise and accessibility.
+      </p>
+      <h4>Mishan Savaliya - Sr.Content Writer</h4>
+      <p>
+        Meet Mishan Savaliya, our role is to share experience and write content
+        with simple langauge that people make understand. With Jenish sir
+        extensive background in Content writer with IT expertise, Jenish sir
+        bring a wealth of knowledge to the forefront. Mishan Savaliya is
+        dedicated to writting blog with simple understandable and many more.
+      </p>
+      <h3>Contact Us</h3>
+      <p>
+        Your feedback is invaluable. If you have questions, seek advice, or want
+        to share your experiences, feel free to reach out to us at
+        bloggersground@gmail.com. We're here to connect and engage with you.
+      </p>
+      <h3>Connect With Us</h3>
+      <p>
+        Stay updated on the latest insights and expert perspectives by following
+        us on twitter-
+        <a href="https://x.com/Bloggers_Ground?t=-nndOfYb4HzPvHxBi-YJwg&s=09">
+          https://x.com/Bloggers_Ground?t=-nndOfYb4HzPvHxBi-YJwg&s=09
+        </a>{" "}
+        instagram-{" "}
+        <a href="https://instagram.com/bloggers_ground?igshid=MmVlMjlkMTBhMg==">
+          https://instagram.com/bloggers_ground?igshid=MmVlMjlkMTBhMg==
+        </a>
+        . Join us in the journey of exploration, learning, and mastery in the
+        fascinating world of BloggersGround. Thank you for being a part of the
+        &apos;BloggersGround.com&apos; community, where expertise meets
+        enthusiasm!
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
