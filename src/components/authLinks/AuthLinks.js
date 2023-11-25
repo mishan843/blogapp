2702a65db9a713ea4c3aea5a77eb108bf385a3f6
Jenish <kavathiyajenish009@gmail.com>
2023-11-25 19:11:12 +0530

    change in meta tag

--- src/components/authLinks/AuthLinks.js
+++ src/components/authLinks/AuthLinks.js
@@ -51,51 +51,51 @@ const AuthLinks = () => {
       )}
 
       <motion.button className={styles.menu} onClick={() => setOpen(!open)} animate={open ? "open" : "closed"}>
-      <svg width="23" height="23" viewBox="0 0 23 23">
-        <motion.path
-          strokeWidth="3"
-          stroke="black"
-          strokeLinecap="round"
-          variants={{
-            closed: { d: "M 2 2.5 L 20 2.5" },
-            open: { d: "M 3 16.5 L 17 2.5" },
-          }}
-        />
-        <motion.path
-          strokeWidth="3"
-          stroke="black"
-          strokeLinecap="round"
-          d="M 2 9.423 L 20 9.423"
-          variants={{
-            closed: { opacity: 1 },
-            open: { opacity: 0 },
-          }}
-        />
-        <motion.path
-          strokeWidth="3"
-          stroke="black"
-          strokeLinecap="round"
-          variants={{
-            closed: { d: "M 2 16.346 L 20 16.346" },
-            open: { d: "M 3 2.5 L 17 16.346" },
-          }}
-        />
-      </svg>
-    </motion.button>
+        <svg width="23" height="23" viewBox="0 0 23 23">
+          <motion.path
+            strokeWidth="3"
+            stroke="black"
+            strokeLinecap="round"
+            variants={{
+              closed: { d: "M 2 2.5 L 20 2.5" },
+              open: { d: "M 3 16.5 L 17 2.5" },
+            }}
+          />
+          <motion.path
+            strokeWidth="3"
+            stroke="black"
+            strokeLinecap="round"
+            d="M 2 9.423 L 20 9.423"
+            variants={{
+              closed: { opacity: 1 },
+              open: { opacity: 0 },
+            }}
+          />
+          <motion.path
+            strokeWidth="3"
+            stroke="black"
+            strokeLinecap="round"
+            variants={{
+              closed: { d: "M 2 16.346 L 20 16.346" },
+              open: { d: "M 3 2.5 L 17 16.346" },
+            }}
+          />
+        </svg>
+      </motion.button>
       {open && (
         <motion.div className={styles.responsiveMenu} variants={variants} animate={open ? "open" : "closed"}>
-        <motion.div className={styles.responsiveLink} >
-          <Link href="/">Home</Link>
-          <Link href="/">About</Link>
-          <Link href="/">Contact</Link>
-          {userData ? (
-            <button className={styles.link} onClick={handleLogout}>
-              Logout
-            </button>
-          ) : (
-            <Link href="/signup" className={styles.link}>Signup</Link>
-          )}
-        </motion.div>
+          <motion.div className={styles.responsiveLink} >
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/">Contact</Link>
+            {userData ? (
+              <button className={styles.link} onClick={handleLogout}>
+                Logout
+              </button>
+            ) : (
+              <Link href="/signup" className={styles.link}>Signup</Link>
+            )}
+          </motion.div>
         </motion.div>
       )}
     </>
