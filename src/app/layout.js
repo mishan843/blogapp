77c48624590e7de4ae8db0d1b7a83036850be745
Jenish <kavathiyajenish009@gmail.com>
2024-01-17 19:04:54 +0530

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
-import GoogleAnalytics from "@/components/googleAny/index";
 
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
